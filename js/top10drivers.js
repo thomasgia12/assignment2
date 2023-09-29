11592c8a9b577df3efc8cb97ae1a9f77d9e102e2
Thomas Gia <thomasgia@Thomass-Air.modem>
2023-09-29 11:07:18 +1000

    Add code

---
+++ js/top10drivers.js
@@ -0,0 +1,47 @@
+const top10drivers = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    "description": "Number of wins of the top 10 drivers from 1950 to 2023",
+    "data": {
+      "url": "https://raw.githubusercontent.com/thomasgia12/assignment2/main/data/combinedexcels.csv",
+      "format": {"type": "csv"}
+    },
+    "transform": [
+      {"filter": "datum.FinalPosition === '1'"},
+      {
+        "aggregate": [{"op": "count", "field": "FinalPosition", "as": "Wins"}],
+        "groupby": ["First", "Last"]
+      },
+      {
+        "window": [{"op": "rank", "as": "rank"}],
+        "sort": [{"field": "Wins", "order": "descending"}]
+      },
+      {"filter": "datum.rank <= 10"}
+    ],
+    "mark": "bar",
+    "encoding": {
+      "x": {
+        "field": "Wins",
+        "type": "quantitative",
+        "axis": {"title": "Number of Wins"}
+      },
+      "y": {
+        "field": "First",
+        "type": "nominal",
+        "axis": {"title": "Driver", "labelAngle": 0},
+        "sort": "-x"
+      },
+      "tooltip": [
+        {"field": "First", "type": "nominal", "title": "Driver"},
+        {"field": "Last", "type": "nominal", "title": "Surname"},
+        {"field": "Wins", "type": "quantitative", "title": "Total Wins"}
+      ]
+    },
+    "config": {}
+  };
+  document.addEventListener("DOMContentLoaded", function() {
+    vegaEmbed('#top10drivers', top10drivers);
+});
+
+  // Use the chartData object in your application
+  console.log(top10drivers.description);  // Outputs: "Number of wins of the top 10 drivers from 1950 to 2023"
+  
